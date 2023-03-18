e0d841e7877683313fa05d0889fc0c839faea78e
正钛汾钸 <121968430+zhengtfb@users.noreply.github.com>
2023-03-19 00:12:38 +0800

    📤缓慢打印
    定义缓慢打印函数

--- meg.py
+++ meg.py
@@ -1,4 +1,5 @@
 import time
+
 #分割字符串
 def fenge(fenge_word):
   fenge_word_n=len(fenge_word)-1
@@ -9,6 +10,7 @@ def fenge(fenge_word):
     fenge_word_n=fenge_word_n-1
   fenge_list1.reverse()
   return fenge_list1
+
 #缓慢打印
 def yanshi(sentence_yanshi,time_sentence_yanshi):
   n=len(sentence_yanshi)
